2534e48ac1d2c163675ca7c217405d73fc14f68c
Tapan Patel <tapanpatel8690@Tapans-MacBook-Air.local>
2025-05-05 18:22:00 +0530

    feedback changes

--- script.js
+++ script.js
@@ -25,6 +25,6 @@ document.getElementById('shape').onclick = function() {
     document.getElementById('shape').style.display = 'none';
     var end = new Date().getTime();
     var timeTaken = (end - start) / 1000;
-    alert(timeTaken);
+    alert(timeTaken + " seconds");
     move();
 }
